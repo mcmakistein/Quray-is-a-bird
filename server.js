b9d02abf1c38c684a64b32ae2dcc98dbb4e36e66
mcmakistein <133424702+mcmakistein@users.noreply.github.com>
2025-12-09 22:11:35 +0300

    Remove comment about changing admin password

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const DB_FILE = 'skorlar.json';
-const ADMIN_PASSWORD = "qurayisbest"; // <-- BURAYA KENDİ ŞİFRENİ YAZ
+const ADMIN_PASSWORD = "qurayisbest"; // 
 
 // Veritabanını Başlat
 let scores = [];
